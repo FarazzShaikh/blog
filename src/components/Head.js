ee5512dcc4e75f13b7259f2c4fbbe0c7cd328544
Faraz Shaikh <farazzshaikh@gmail.com>
2021-07-21 10:00:18 +0530

    patches

--- src/components/Head.js
+++ src/components/Head.js
@@ -23,8 +23,6 @@ export const Head = ({ description, lang, meta, image: metaImage, title, pathnam
   const image = metaImage && metaImage.images && metaImage.images.fallback && metaImage.images.fallback.src ? `${site.siteMetadata.siteUrl}${metaImage.images.fallback.src}` : null;
   const canonical = pathname ? `${site.siteMetadata.siteUrl}${pathname}` : null;
 
-  console.log(image);
-
   const icon = glyph || "📖";
   const iconHref = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${icon}</text></svg>`;
 
